4c29206970ab368f5959b900f5a24f68dee288a0
Karthikeyan <karthikeyan@bangthetable.com>
2021-05-08 13:15:01 +0530

    fix: setting up the purge css in post config css

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,28 @@
 import Dotenv from "dotenv-webpack";
-export default (config) => {
+
+module.exports = (config, env, helpers, params = defaultParams) => {
   config.plugins.push(new Dotenv({path: "./.env"}));
   // process is not defined
   config.node.process = 'mock';
+  const purgecss = require('@fullhuman/postcss-purgecss')({
+    // Specify the paths to all of the template files in your project
+    content: ['./src/**/*.js'],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(params.regex) || [],
+  });
+
+  const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
+  postCssLoaders.forEach(({ loader }) => {
+    const plugins = loader.options.plugins;
+
+    // Add tailwind css at the top.
+    plugins.unshift(require('tailwindcss'));
+
+    // Add PurgeCSS only in production.
+    // if (env.production) {
+      plugins.push(purgecss);
+    // }
+  });
+  return config;
 };
