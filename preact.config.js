61b12d81bc4480c16bcd0232dfd769d1bdec9ef4
Karthikeyan <karthikeyan@bangthetable.com>
2021-05-08 22:01:04 +0530

    fix: adding purge only for production

--- preact.config.js
+++ preact.config.js
@@ -5,7 +5,7 @@ export default (config, env, helpers, params = defaultParams) => {
   config.node.process = 'mock';
   const purgecss = require('@fullhuman/postcss-purgecss')({
     // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
+    content: ['./src/**/*.html', './src/**/*.js',],
 
     // Include any special characters you're using in this regular expression
     defaultExtractor: content => content.match(params.regex) || [],
@@ -18,6 +18,10 @@ export default (config, env, helpers, params = defaultParams) => {
     // Add tailwind css at the top.
     plugins.unshift(require('tailwindcss'));
 
+    // Add PurgeCSS only in production.
+    if (env.production) {
+      plugins.push(purgecss);
+    }
   });
   return config;
 };
